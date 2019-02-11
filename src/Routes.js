2ff403a96a07a74e9b56525455ac61bf99446d8a
Henry Soule <hsoule427@gmail.com>
2019-02-11 11:41:36 -0700

    Add DiagnosisChart

--- src/Routes.js
+++ src/Routes.js
@@ -1,10 +1,12 @@
 import React from "react";
 import { Route, Switch } from "react-router-dom";
 import Home from "./Views/Home";
+import DiagnosisChart from "./Views/DiagnosisChart";
 import NotFound from "./Views/NotFound";
 
 export default () =>
   <Switch>
     <Route path="/" exact component={Home} />
+    <Route path="/DiagnosisChart" exact component={DiagnosisChart} />
     <Route component={NotFound} />
   </Switch>;
