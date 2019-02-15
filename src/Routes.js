3280822f9a14d3f1ccc378abe9df788f6b96fb01
George <GeoEngel.z@gmail.com>
2019-02-14 21:47:45 -0700

    Added the basic GUI implementation for Signing up
    -Added signup page
    -signup.js & signup.css
    -Also added loading button component

--- src/Routes.js
+++ src/Routes.js
@@ -4,11 +4,13 @@ import Home from "./Views/Home";
 import NotFound from "./Views/NotFound";
 import docs from "./Views/docs";
 import Login from "./Views/Login";
+import Signup from "./Views/Signup";
 
 export default () =>
   <Switch>
     <Route path="/" exact component={Home} />
     <Route path="/login" exact component={Login} />
+    <Route path="/signup" exact component={Signup} />
     <Route path="/docs" exact component={docs} />
     <Route component={NotFound} />
   </Switch>;
