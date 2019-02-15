ab8f040cc4fcd6655aa70046e307c3da0af38f00
Henry Soule <hsoule427@gmail.com>
2019-02-15 07:03:51 -0700

    Complete template for Diagnosis Chart

--- src/Views/DiagnosisChart.js
+++ src/Views/DiagnosisChart.js
@@ -13,43 +13,109 @@ class DiagnosisChart extends Component {
 	render() {
 		const {classes} = this.props;
 		const {spacing} = this.state;
-		console.log(this.props);
 		return (
 			<div className='DiagnosisChart'>
 				<Grid justify='center' container spacing={24}>
-					<Grid item xs={12}>
-						<Paper>
-							<h2>Diagnosis of Depression</h2>
-						</Paper>
+					<Grid justify='center' container spacing={24}>
+						<Grid item xs={12}>
+							<Paper>
+								<h2>Diagnosis of Depression</h2>
+							</Paper>
+						</Grid>
+						<Grid item xs={4}>
+							<Paper>
+								<p>Initial therapy with citalopram or sertaline (unless compelling indication for alternate agent).</p>
+								<p>Address side effects and encourage adherence in 1 week. Evaluate response in 3-4 weeks.</p>
+							</Paper>
+						</Grid>
 					</Grid>
-					<Grid item xs={4}>
-						<Paper>
-							<p>Initial therapy with citalopram or sertaline (unless compelling indication for alternate agent).</p>
-							<p>Address side effects and encourage adherence in 1 week. Evaluate response in 3-4 weeks.</p>
-						</Paper>
+
+					<Grid justify='center' container spacing={24}>
+						<Grid item xs={4}>
+							<Paper>
+								<h4>Non Response</h4>
+								<p>Ensure medication adherence.</p>
+								<p>Optimize dose <strong>OR</strong> switch (alternate SSRI or non-SSRI)</p>
+							</Paper>	
+						</Grid>
+						
+						<Grid item xs={4}>
+							<Paper>
+								<h4>Partial Response</h4>
+								<p>Optimize dose <strong>OR</strong> augment</p>
+							</Paper>
+						</Grid>
+
+						<Grid item xs={4}>
+							<Paper>
+								<h4>Full Response</h4>
+								<p>Continue same treatment for at least 4-9 months</p>
+							</Paper>
+						</Grid>
+					</Grid>
+
+					<Grid container spacing={24}>
+						<Grid item xs={4}>
+							<Paper>
+								<p>Evaluate response in 3-4 weeks.</p>
+							</Paper>
+						</Grid>
+
+						<Grid item xs={4}>
+							<Paper>
+								<p>Evaluate response in 3-4 weeks.</p>
+							</Paper>
+						</Grid>
+					</Grid>
+
+					<Grid container spacing={24}>
+						<Grid item xs={4}>
+							<Paper>
+								<h4>Non Response</h4>
+								<p>Switch to a different antidepressant</p>
+								<p>(SSRI or non-SSRI)</p>
+							</Paper>
+						</Grid>
+						
+						<Grid item xs={4}>
+							<Paper>
+								<h4>Partial Response</h4>
+								<p>Optimize dose <strong>OR</strong> augment <strong>OR</strong></p>
+							</Paper>
+						</Grid>
+
+						<Grid item xs={4}>
+							<Paper>
+								<h4>Full Response</h4>
+								<p>Continue same treatment for at least 4-9 months</p>
+							</Paper>
+						</Grid>
+					</Grid>
+
+					<Grid container spacing={24}>
+						<Grid item xs={4}>
+							<Paper>
+								<h4>Non Response</h4>
+								<p>Switch to a different antidepressant</p>
+								<p>(alternate SSRI or non-SSRI)</p>
+							</Paper>
+						</Grid>
+						
+						<Grid item xs={4}>
+							<Paper>
+								<h4>Partial Response</h4>
+								<p>Optimize does <strong>OR</strong> augment <strong>OR</strong> switch</p>
+							</Paper>
+						</Grid>
+
+						<Grid item xs={4}>
+							<Paper>
+								<h4>Full Response</h4>
+								<p>Continue same treatment for at least 4-9 months</p>
+							</Paper>
+						</Grid>
 					</Grid>
 				</Grid>
-
-				<Grid justify='center' container spacing={24}>
-					<Grid item xs={4}>
-						<Paper>
-							Item 1.
-						</Paper>	
-					</Grid>
-					
-					<Grid item xs={4}>
-						<Paper>
-							Item 2.
-						</Paper>
-					</Grid>
-
-					<Grid item xs={4}>
-						<Paper>
-							Item 3. 
-						</Paper>
-					</Grid>
-				</Grid>
-
 			</div>
 		);
 	}
