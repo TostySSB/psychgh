ced8019b5afa59228eb472ad64a3d3e2f5980d43
Unknown <toster1011@gmail.com>
2019-02-15 00:15:18 -0700

    Hotfix: part 3 of heroku deployment fix

--- index.js
+++ index.js
@@ -3,20 +3,9 @@ const path = require('path');
 
 const app = express();
 
-// Serve the static files from the React app
-app.use(express.static(path.join(__dirname, 'client/build')));
-
-// An api endpoint that returns a short list of items
-app.get('/api/getList', (req,res) => {
-    var list = ["item1", "item2", "item3"];
-    res.json(list);
-    console.log('Sent list of items');
-});
-
 // Handles any requests that don't match the ones above
 //Static file declaration
 app.use(express.static(path.join(__dirname, 'client/build')));
-
 //production mode
 if(process.env.NODE_ENV === 'production') {
   app.use(express.static(path.join(__dirname, 'client/build')));
@@ -34,3 +23,9 @@ const port = process.env.PORT || 5000;
 app.listen(port);
 
 console.log('App is listening on port ' + port);
+// An api endpoint that returns a short list of items
+app.get('/api/getList', (req,res) => {
+    var list = ["item1", "item2", "item3"];
+    res.json(list);
+    console.log('Sent list of items');
+});
