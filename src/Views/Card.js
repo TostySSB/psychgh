2d6e870aba42a68486d3b1bffc20c2bbf385bb31
Henry Soule <hsoule427@gmail.com>
2019-02-28 20:23:11 -0700

    Add DiagnosisChartMenu and Card

---
+++ src/Views/Card.js
@@ -0,0 +1,5 @@
+import React, {Component} from 'react';
+import Input from '../components/UI/Input'
+import Grid from '@material-ui/core/Grid';
+import { withStyles } from '@material-ui/core/styles';
+import Paper from '@material-ui/core/Paper';
