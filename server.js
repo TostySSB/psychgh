5837c044093df9f07409b3d970350bccf9361074
Unknown <toster1011@gmail.com>
2019-03-01 23:33:20 -0700

    Heroku hotfix, added production env config

--- server.js
+++ server.js
@@ -35,7 +35,14 @@ require("./config/passport")(passport);
 
 // Routes
 app.use("/api/users", users);
+if (process.env.NODE_ENV === 'production') {
+  // Set static folder
+  app.use(express.static('client/build'));
 
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+  });
+}
 const port = process.env.PORT || 5000;
 
 app.listen(port, () => console.log(`Server up and running on port ${port} !`));
