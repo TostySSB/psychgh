5cd9066ace14c52f5a4e4f42105a74ac8de07609
George <GeoEngel.z@gmail.com>
2019-03-26 23:41:06 -0600

    Added component notes to patient evaluation for future reference

--- client/src/Views/Patient_evaluation.js
+++ client/src/Views/Patient_evaluation.js
@@ -1,5 +1,13 @@
 import React, { Component } from 'react';
 
+// This Page displays an evaluation test to a patient
+
+// Creates an instance of the form specified from the DB and serves it to the patient
+
+// Handle creation of a report based on this form once it is finished on the server side and store in the DB under the patient's results
+
+// Redirect the patient to that new results page with option to notify their practitioner
+
 class Patient_evaluation extends Component{
     render(){
         return(
@@ -10,3 +18,16 @@ class Patient_evaluation extends Component{
     }
 }
 export default Patient_evaluation;
+
+/*Currently working on:
+	-Making an abstract component for creating forms for patient evaluation tests
+
+	WILL NEED (in form): PHQ maker
+		-Tables that contain rows/columns of 'components'
+		-Text input boxes
+		-grouped check boxes (can only select from one of X check boxes)
+		-check boxes (multiple choice)
+		-Date input
+		-requirement flag for all above (marked with red asterisks
+
+	General document creator/editor for medication details (effects, studies, stats)/*
