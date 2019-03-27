23c4cecc90c9c42a046101c9e0ebea4c624a4599
George <GeoEngel.z@gmail.com>
2019-03-26 23:29:27 -0600

    Added pages for patient evaluation test creation

---
+++ client/src/Views/Patient_Eval_Maker.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+// TODO: Make components for evaulation creator (with editable feilds, then export them to json formatt for the database)
+// TODO: Add drag and drop as well as ADD/ REMOVE functionality for each component
+class Patient_Eval_Maker extends Component{
+    render(){
+        return(
+            <div className="Patient_Eval_Maker">
+                
+            </div>
+        );
+    }
+}
+export default Patient_Eval_Maker;
---
+++ client/src/Views/Patient_evaluation.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+
+class Patient_evaluation extends Component{
+    render(){
+        return(
+            <div className="Patient_evaluation">
+                
+            </div>
+        );
+    }
+}
+export default Patient_evaluation;
