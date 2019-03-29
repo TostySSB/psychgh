343a78917c66d6e40d286d27b9e8da80b7bc03f1
Henry Soule <hsoule427@gmail.com>
2019-03-29 15:39:11 -0600

    Edited login test

--- client/src/components/auth/__tests__/Login.test.js
+++ client/src/components/auth/__tests__/Login.test.js
@@ -1,9 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import Login from '../Login'
+import Login from '../Login';
+import Adapter from 'enzyme-adapter-react-16';
 import { shallow, configure } from 'enzyme';
 
-configure({adapter: new })
+configure({adapter: new Adapter()})
 describe('Login Component renders', () => {
 	it('Login renders without crashing', () => {
 		shallow(<Login />);
